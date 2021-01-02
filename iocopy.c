207ef12f80332f1a7ba29d613b3bbd1321b7ede1
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-01-01 20:03:26 -0800

    Write iocopy.c

---
+++ iocopy.c
@@ -0,0 +1,17 @@
+/* This program is an example of an input-output computation */
+/* A separate file will be written to compare the logic of both */
+
+#include <stdio.h>
+
+/* Copy input to output; Optimized version */
+main()
+{
+   int c;
+   
+   while ((c = getchar()) != EOF)
+      putchar(c);
+      
+ }
+ 
+ /* Please note this is not my program */
+ /* Comments were added although it was copied from Dennis Ritchie/Ken Thompson's "C Programming Language" */
