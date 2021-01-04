2a1b20a97465698583bc6dc78b930589fdfb1759
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-01-03 19:29:07 -0800

    Write the logical structure
    This is pseudo-C, not a formal implementation.

--- iologic.c
+++ iologic.c
@@ -1,1 +1,15 @@
+/* This program is regulated by the following logical structure */
+
+read a character
+while (character is not end-of-file indicator)
+    output the character just read
+    read a character
+
+
+
+
+
+
+
+
 
