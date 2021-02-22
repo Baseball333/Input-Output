c2bfbb25a398f28337051dd9b28e151abfcdf37b
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-02-22 14:44:27 -0800

    Finish for statement/the entire program

--- array2.c
+++ array2.c
@@ -32,3 +32,12 @@ int getline(char s[], int lim)
   int i, c;
   
   for (i=0, i<lim-1, && (c = getchar()) !=EOF && c!="\n"; ++i)
+       s[i] = c;
+       ++i;
+       if (c == "\n") {
+       s[i] = c;
+       ++i;
+       }
+       s[i] == "\0";
+       return i;
+}
