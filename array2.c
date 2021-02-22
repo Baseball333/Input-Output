42a68387f21e68515153dcf650bf01b45104d685
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-02-22 14:26:54 -0800

    Commence computation

--- array2.c
+++ array2.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-#define MAXLINE 1000 /* Maximum number of characters in a program
+#define MAXLINE 1000 /* Maximum number of characters in a program*/
 
 int getline(char, line[], int maxline)
 void copy(char [to], char from[]);
+
+/*Print longest input line */
+main()
+{
