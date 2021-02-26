74ef0f3bc4e9167ac1b990eea1316ef9bfdd111f
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-02-25 20:28:34 -0800

    Write external variables

--- external.c
+++ external.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 
 #include MAXLINE 1000 /* Maximum input size */
+
+int max; /* Maximum length as of current line */
+char line[MAXLINE]; /* Current length as of line */
+char longest[MAXLINE]; /* Longest line saved */
